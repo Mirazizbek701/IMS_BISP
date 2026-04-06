6b6dbf4b1db38591ab4bcb7f5d42d8171ccf8c2f
Miraziz <mirazizbek701@gmail.com>
2026-04-06 08:37:51 +0500

    Stor and User Repositories completed

---
+++ IMS_BISP.DAL/Extensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.SqlClient;
+
+namespace IMS_BISP.DAL
+{
+    public static class Extensions
+    {
+        public static bool HasColumn(this SqlDataReader reader, string columnName)
+        {
+            for (int i = 0; i < reader.FieldCount; i++)
+                if (reader.GetName(i) == columnName) return true;
+            return false;
+        }
+    }
+}
